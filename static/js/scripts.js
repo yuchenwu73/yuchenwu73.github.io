6715987210ba18bd9d3774583e1ffe73faa9d665
yuchenwu <ycwu73@163.com>
2024-10-29 03:26:22 +0800

    Revert "refactor(yuchenwu73.github.io): 优化 CV 下拉菜单在移动端和桌面端的显示逻辑"
    This reverts commit 11ec639e1657ae76c7bb2ecd45ea46e997822bd5.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -64,17 +64,16 @@ window.addEventListener('DOMContentLoaded', event => {
 
 }); 
 
-
 // 确保DOM加载完成后才初始化
 document.addEventListener('DOMContentLoaded', () => {
     // 查找所有具有dropdown-toggle类的元素
-    const dropdown = document.querySelector('#navbarDropdownCV');
-
-    if (window.innerWidth < 992) { // 仅在手机端执行
+    const dropdowns = document.querySelectorAll('.dropdown-toggle');
+    dropdowns.forEach(dropdown => {
         dropdown.addEventListener('click', function (e) {
-            e.preventDefault(); // 阻止默认行为
-            this.nextElementSibling.classList.toggle('show'); // 切换显示状态
+            // 阻止默认点击事件，确保在手机端正常展开
+            e.preventDefault();
+            // 切换当前菜单的显示状态
+            this.nextElementSibling.classList.toggle('show');
         });
-    }
+    });
 });
-
